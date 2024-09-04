9eb1890b0ae80fd16595105166037c3d409f08df
Zain Ul Islam <145145040+zainexperience0@users.noreply.github.com>
2024-09-04 10:37:50 +0500

    docs api done

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -25,6 +25,8 @@ export async function GET(req: Request) {
       scope: [
         "https://www.googleapis.com/auth/blogger",
         "https://www.googleapis.com/auth/drive",
+        "https://www.googleapis.com/auth/drive.file",
+        "https://www.googleapis.com/auth/documents",
         "https://www.googleapis.com/auth/drive.apps.readonly",
         "https://www.googleapis.com/auth/calendar.readonly",
         "https://www.googleapis.com/auth/calendar.events",
